aef6e71776caa193993e6e8e2dd5667ca1e481af
dawmsi <da._.off@outlook.com>
2024-01-09 20:38:29 +0200

    select textures & add text

--- src/components/AppTree.svelte
+++ src/components/AppTree.svelte
@@ -1,10 +1,32 @@
 <script>
   import { Canvas, T } from "@threlte/core";
-  import { GLTF, OrbitControls } from "@threlte/extras";
+  import { OrbitControls, Text, useGltf } from "@threlte/extras";
+
+  const models = {
+    tv: "tv",
+    car: "car",
+    car2: "che",
+  };
+
+  let text = "top right deg for select ^";
+  let fontSize = 2;
+
+  let select = models[1];
 </script>
 
 <div class="app">
+  <select name="models" id="models" bind:value={select}>
+    {#each Object.keys(models) as key}
+      <option value={models[key]}>{key}</option>
+    {/each}
+  </select>
   <Canvas rendererParameters={{ antialias: true }}>
+    {#await useGltf(`/models/${select}.glb`) then gltf}
+      <T is={gltf.scene} position.y={0.02} scale={select === "car" ? 0.1 : 1} />
+    {/await}
+
+    <Text {text} color="white" {fontSize} anchorY={-17} />
+
     <T.PerspectiveCamera makeDefault position={[10, 20, 40]}>
       <OrbitControls
         autoRotate={false}
@@ -19,16 +41,22 @@
     </T.PerspectiveCamera>
     <T.AmbientLight intensity={7} />
     <T.DirectionalLight position.y={20} position.z={-50} intensity={2} />
-    <GLTF url="/car.glb" position.y={0.02} scale={0.1} />
-    <T.GridHelper args={[200, 100]} />
+
+    <T.GridHelper args={[200, 100]} receiveShadow />
   </Canvas>
 </div>
 
 <style>
   .app {
-    position: absolute;
+    position: relative;
     height: 100%;
     width: 100%;
     background: radial-gradient(#103272, #317c7c);
+  }
+  select {
+    position: absolute;
+    margin-inline: auto;
+    right: 0;
+    margin: 15px;
   }
 </style>
